983f3585156ea87d97632c934f7e5e9fe0f465da
LouisSetyandaru <121075836+LouisSetyandaru@users.noreply.github.com>
2025-05-27 16:08:11 +0700

    Setup Database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 // Replace with your actual contract address
-const CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 // Your contract ABI - replace with the actual ABI
 const CarRentalABI = [
