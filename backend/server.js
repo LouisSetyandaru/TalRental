8b17b3a6d370d208f539713ff85be6615508e130
DhyasR <119585072+DhyasR@users.noreply.github.com>
2025-06-09 20:08:19 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 const path = require('path');
 app.use('/metadata', express.static(path.join(__dirname, 'metadata')));
